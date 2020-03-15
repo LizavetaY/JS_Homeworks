84c63d803ab42bf8cbc6b3313369a0046f4fa5fd
LizavetaY <47990337+LizavetaY@users.noreply.github.com>
2020-03-15 22:38:28 +0300

    HW_9_Object Animal and Object Cat

---
+++ hw9/main.js
@@ -0,0 +1,61 @@
+function Animal(name) {
+    var foodAmount = 50;
+
+    this.name = name;
+
+    this.feed = function () {
+        console.log('Насыпаем в миску ' + this.dailyNorm() + ' корма');
+    }
+
+    function formatFoodAmount() {
+        return foodAmount + ' гр.';
+    }
+
+    this.dailyNorm = function (amount) {
+        if (!arguments.length) return formatFoodAmount();
+
+        if (amount < 50 || amount > 500) {
+            throw new Error('Неверный объем');
+        }
+
+        foodAmount = amount;
+    }
+}
+
+function Cat(name) {
+    Animal.call(this);
+
+    this.name = name;
+
+    var animalFeed = this.feed;
+
+    this.feed = function () {
+        animalFeed.call(this);
+        console.log('Кот доволен ˆ_ˆ');
+        return this;
+    }
+
+    this.stroke = function () {
+        console.log('Гладим кота.');
+        return this;
+    }
+}
+
+console.log('***Object Animal***');
+var kitty = new Animal('Kitty');
+kitty.dailyNorm(78);
+console.log(kitty);
+console.log(kitty.feed());
+console.log(kitty.dailyNorm());
+
+// TASK 4
+console.log('***Object Cat***');
+var barsik = new Cat('Barsik');
+barsik.dailyNorm(66);
+console.log(barsik);
+console.log(barsik.feed());
+console.log(barsik.dailyNorm());
+
+// TASK 5
+console.log('***Chaining***');
+barsik.stroke().feed().stroke().stroke().feed();
